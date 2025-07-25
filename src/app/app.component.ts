6e6333f3b1519703b8e72244db2002f230f235ea
rshyam@hala.uk <rshyam@hala.uk>
2025-07-25 18:40:20 +0530

    minor changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'azure-angular-app-demo';
+  title = 'azure-angular-app-deploy-demo';
 }
